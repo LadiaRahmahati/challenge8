ba8628fea1d1918b4f5f40f455e01d65e3dc6489
Ladia Rahmahati <ladiarahmahati@students.unnes.ac.id>
2022-06-09 20:35:10 +0700

    update config

--- config/database.js
+++ config/database.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 const {
-  DB_USER = "",
-  DB_PASSWORD = "",
-  DB_NAME = "bcr",
-  DB_HOST = "127.0.0.1",
-  DB_PORT = "5432",
+  DB_USER = process.env.DB_USER,
+  DB_PASSWORD = process.env.DB_PASSWORD,
+  DB_NAME = process.env.DB_NAME,
+  DB_HOST = process.env.DB_HOST,
+  DB_PORT = process.env.DB_PORT,
 } = process.env;
 
 module.exports = {
@@ -13,7 +15,13 @@ module.exports = {
     database: `${DB_NAME}_development`,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
   test: {
     username: DB_USER,
@@ -29,6 +37,12 @@ module.exports = {
     database: `${DB_NAME}_production`,
     host: DB_HOST,
     port: DB_PORT,
-    dialect: "postgres"
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
 }
